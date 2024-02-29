54d2a45d32da15dd6b1ba434684755311d66377f
Esther Salamon <esther386386@gmail.com>
2024-02-28 21:16:58 -0500

    Add project files.

---
+++ ToPushOntoGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToPushOntoGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is what we will push onto github. Would you like to see?");
+        }
+    }
+}
